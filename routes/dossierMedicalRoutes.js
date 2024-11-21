7d68d7217d3b79c75320c3339870f876154970c7
Abdoulaye95678-cloud <elhadj3002@gmail.com>
2024-11-21 14:06:35 -0500

    Mise a jour de route dossierMedical

--- routes/dossierMedicalRoutes.js
+++ routes/dossierMedicalRoutes.js
@@ -3,10 +3,10 @@ import { dossierMedicalList, addDossierMedical, getDossierMedicalById, updateDos
 
 const route= Router();
 
-route.get('/', dossierMedicalList);
-route.post('/', addDossierMedical);
-route.get('/:id', getDossierMedicalById);
-route.put('/:id', updateDossierMedical);
-route.delete('/:id', deleteDossierMedical);
+route.get('/', dossierMedicalList);//recupérer la liste de tout les dossiers médicaux
+route.post('/', addDossierMedical);//Ajout d'un dossier medicale
+route.get('/:id', getDossierMedicalById);///recupérer la liste de tout les dossiers médicaux par ID
+route.put('/:id', updateDossierMedical);// mise a jour du dossier medical
+route.delete('/:id', deleteDossierMedical);// supprimer le dossier medical
 
 export default route;
